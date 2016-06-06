af4c8bbc87e271ad2d1f1c83b4458d48d7ec6a31
10416004 <sam.js111@yahoo.com.tw>
2016-06-06 15:25:06 +0800

    TestBinaryTree
    do the binary sort

---
+++ TestBinaryTree.java
@@ -0,0 +1,136 @@
+//U10416004 顏仲賢
+import java.security.SecureRandom;
+
+
+public class BinaryTree {
+	private int size = 0;
+	private Node root;
+
+	//create a no argument constructor
+	public BinaryTree(){}
+
+	//create a constructor
+	public BinaryTree(int[] num) {
+		
+		//add number into this array
+		for(int i = 0; i < num.length; i++) {
+			add(num[i]);
+		}
+	}
+	
+	public void add(int element) {
+		
+		if(size == 0){
+			root = new Node(element);
+			size++;
+		}
+		
+		//if the next number is big than previous,then let these number change their site
+		else{
+			if(element < (int)root.element) {
+				addToLeft(element,root);
+			}
+			
+			else{
+				addToRight(element,root);
+			}
+		}
+	}
+	
+	//if the number is smaller add this number to left
+	public void addToLeft(int element, Node temp) {
+		if(temp.left == null) {
+			temp.left = new Node(element);
+			size++;
+		}
+		
+		else if(element < (int)((temp.left).element)){
+			addToLeft(element, temp.left);
+		}
+		
+		else{
+			addToRight(element, temp.left);
+		}
+	}
+
+	//if the number is larger add this number to right
+	public void addToRight(int element, Node temp) {
+		if(temp.right == null) {
+			temp.right = new Node(element);
+			size++;
+		}
+		
+		else if(element > (int)((temp.right).element)) {
+			addToRight(element, temp.right);
+		}
+		
+		else{
+			addToLeft(element, temp.right);
+		}
+	}
+	
+	//print the result of binary tree
+	public void printOut() {
+		if(root.left != null) {
+			printOut(root.left);
+		}
+		
+		System.out.println(root.element);
+		
+		if(root.right != null) {
+			printOut(root.right);
+		}
+	}
+
+	//print the result
+	public void printOut(Node node) {
+		if(node.left != null) {
+			printOut(node.left);
+		}
+		
+		System.out.println(node.element);
+		
+		if(node.right != null) {
+			printOut(node.right);
+		}
+	}
+
+	
+	private static class Node<E> {
+		E element;
+		Node<E> left;
+		Node<E> right;
+
+		public Node(){}
+
+		public Node(E element){
+			this.element = element;
+		}
+	}
+}
+
+public class TestBinaryTree {
+	public static void main(String[] args) {
+
+		//create a array to store 20 number which the user want to do the binary tree
+		int[] number = new int[20];
+
+		SecureRandom random = new SecureRandom();
+
+		//add the random number in the array
+		for(int i = 0; i < 20; i++) {
+			number[i] = random.nextInt(100);
+			
+			for(int j = 0; j < i - 1; j++) {
+				if(number[i] == number[j]) {
+					i--;
+				}
+			}
+		}
+
+		BinaryTree b = new BinaryTree(number);
+
+		b.printOut();
+	}
+
+}
